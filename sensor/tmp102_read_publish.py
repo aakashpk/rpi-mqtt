011d66a5ab3b5afbdb5843a9667cd1c2b0b4e4ee
Aakash <aakash.kumar@colorado.edu>
2025-04-10 03:42:22 -0400

    add systemd-pythond to requirements and enhance TMP102 script for Raspberry Pi compatibility

--- sensor/tmp102_read_publish.py
+++ sensor/tmp102_read_publish.py
@@ -15,14 +15,15 @@ parser.add_argument('--topic', default='sensors/temperature', help='MQTT topic t
 parser.add_argument('--serial_number', default='xxxx', help='unique sensor serial number')
 args = parser.parse_args()
 
-# I2C and TMP102 setup
-bus = smbus.SMBus(1)
-TMP102_ADDRESS = 0x48
-
-# Setup logging to systemd journal
-logger = logging.getLogger('tmp102_sensor')
-logger.addHandler(JournalHandler())
-logger.setLevel(logging.INFO)
+# Check if the script is running on a Raspberry Pi
+def is_raspberry_pi():
+    """Check if the script is running on a Raspberry Pi."""
+    try:
+        with open('/proc/cpuinfo', 'r') as f:
+            cpuinfo = f.read()
+        return 'Raspberry Pi' in cpuinfo or 'BCM' in cpuinfo
+    except FileNotFoundError:
+        return False
 
 def is_tmp102_connected():
     try:
@@ -44,16 +45,33 @@ def on_disconnect(client, userdata, rc):
         logger.error("Unexpected disconnection from MQTT broker.")
         print("Error: Unexpected disconnection from MQTT broker.")
 
-# MQTT Setup
-mqtt_client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)
-mqtt_client.connect(args.broker, args.port, 60)
-mqtt_client.loop_start()
+# Setup logging to systemd journal
+logger = logging.getLogger('tmp102_sensor')
+logger.addHandler(JournalHandler())
+logger.setLevel(logging.INFO)
+
+# Check if running on Raspberry Pi
+if not is_raspberry_pi():
+    error_message = "Error: This script must be run on a Raspberry Pi."
+    print(error_message)
+    logger.error(error_message)
+    exit(1)
+
+# I2C and TMP102 setup
+bus = smbus.SMBus(1)
+TMP102_ADDRESS = 0x48
 
 if not is_tmp102_connected():
     error_message = "Error: TMP102 sensor not detected. Please check the connection."
     print(error_message)  # Print the error message to the console
     logger.error(error_message)  # Log the error to the journal
     exit(1)
+
+# MQTT Setup
+mqtt_client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)
+mqtt_client.connect(args.broker, args.port, 60)
+mqtt_client.on_disconnect = on_disconnect
+mqtt_client.loop_start()
 
 try:
     while True:
